0ce458c2f5c159bc92af95f5da4116c52bc4a5d9
preetygoyal <104938943+preetygoyal@users.noreply.github.com>
2024-02-18 20:16:58 +0000

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-allow-all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"  
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.example.name]
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
